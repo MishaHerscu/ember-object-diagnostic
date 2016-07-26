b2fddf2372d28faa4aaa0b79cb8bb34f4911da25
MishaHerscu <mishaherscu@gmail.com>
2016-07-26 09:34:38 -0400

    response

--- public/diagnostic.js
+++ public/diagnostic.js
@@ -23,3 +23,54 @@
 //  -  Order 1 : 2 hats ($5 each)
 //  -  Order 2 : 1 desk lamp ($20 each)
 //  -  Order 3 : 3 hand towels ($8 each)
+
+import Ember from 'ember';
+
+const Order = Ember.Object.extend({
+  orderPrice: Ember.computed('price', 'quantity', function(){
+  return this.get('price') * this.get('quantity');
+  })
+});
+
+const Cart = Ember.Object.extend({
+  addToCart: function(item){
+    this.orders.push(item);
+    return this.get('orders');
+  },
+  orders: [],
+  totalPrice: Ember.computed('orders.@each.orderPrice', function(){
+    let orders = this.get('orders');
+    return orders.reduce((prev, curr) => {
+      return prev + curr.orderPrices;
+    },0);
+  })
+});
+
+let exampleCart = Cart.create({
+  orders:[
+    Order.create(
+      {
+        item: 'hat',
+        price: 5,
+        quantity: 2
+      }
+    ),
+    Order.create(
+      {
+        item: 'desk lamp',
+        price: 20,
+        quantity: 1
+      }
+    ),
+    Order.create(
+      {
+        item: 'hand towels',
+        price: 8,
+        quantity: 3
+      }
+    )
+  ]
+});
+
+console.log(exampleCart);
+console.log(exampleCart.totalPrice);
